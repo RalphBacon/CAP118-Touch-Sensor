02c3e05e5a1d4d496a82eb7b8eb6c123aa3083c8
Ralph Bacon <arduino@jargongeneration.uk>
2017-09-14 19:37:16 +0100

    Sketch for a ProMicro as a keyboard emulator

---
+++ USB_HID_ProMicro.ino
@@ -0,0 +1,23 @@
+#include <Keyboard.h>
+
+int buttonPin = 9;  // Set a button to any pin
+
+void setup()
+{
+  //pinMode(buttonPin, INPUT);  // Set the button as an input
+  //digitalWrite(buttonPin, HIGH);  // Pull the button high
+}
+
+void loop()
+{
+  int inPin = analogRead(A0);
+  //Serial.println(inPin);
+  if ( inPin > 0)
+  {
+    Keyboard.write('A');  // send a 'z' to the computer via Keyboard HID
+    buttonPin++;
+  }
+
+  delay(250);
+}
+
